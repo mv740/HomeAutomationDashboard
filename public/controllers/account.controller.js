80122568d303ed6c3af50401298dcbac49cd8982
michal <michalwozniak@live.ca>
2016-01-16 14:24:16 -0500

    added comfirm/cancel step for the  password/username update process in Account setting

--- public/controllers/account.controller.js
+++ public/controllers/account.controller.js
@@ -17,7 +17,7 @@
     function AccountController(AuthenticationService) {
         var vm = this;
         vm.account = {
-            username : AuthenticationService.getUserName()
+            username: AuthenticationService.getUserName()
         };
 
         vm.tabs = [{
@@ -34,20 +34,55 @@
             vm.currentTab = tab.url;
         };
 
-        vm.isActiveTab = function(tabUrl) {
+        vm.isActiveTab = function (tabUrl) {
             return tabUrl === vm.currentTab;
         };
 
         vm.activePanel = 'public/views/partials/account-username.html';
 
-        vm.showPanel = function(url) {
+        vm.showPanel = function (url) {
             vm.activePanel = url;
         };
 
-        vm.usernameUpdate = function(account)
-        {
-            console.log(account);
+        vm.usernameUpdate = function (account) {
             AuthenticationService.account.usernameUpdate(account);
+            vm.confirmShow.username.resultU = false;
+        };
+
+        vm.passwordUpdate = function (account) {
+            AuthenticationService.account.passwordUpdate(account);
+            vm.confirmShow.password.resultP = false;
+            vm.account.password = null;
+            vm.account.confirmPassword = null;
+        };
+
+        vm.confirmShow = {
+            password: {
+                resultP: false
+            },
+            username: {
+                resultU: false
+            }
+        };
+
+        vm.confirm = function (data) {
+            if(data.hasOwnProperty('resultP')){
+                vm.confirmShow.password.resultP = true;
+            }
+            if(data.hasOwnProperty('resultU'))
+            {
+                vm.confirmShow.username.resultU = true;
+            }
+        };
+
+        vm.cancel = function (data) {
+            if(data.hasOwnProperty('resultP')){
+                vm.confirmShow.password.resultP = false;
+            }
+            if(data.hasOwnProperty('resultU'))
+            {
+                vm.confirmShow.username.resultU = false;
+            }
         };
     }
 })();
